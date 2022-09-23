b9c91ae61983ca247025516756e4c4e4ddeb0170
Jacques <j.nguyen14216@gmail.com>
2022-09-23 22:16:48 +0200

    new component Carousel

--- src/app/05.Logement/Logement.jsx
+++ src/app/05.Logement/Logement.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { useSearchParams } from 'react-router-dom'
+import Carousel from '../../component/Carousel';
+import logements from "../../data/logements.json" 
 import { route } from '../../utils/route';
 
 export default function Logement () {
     const [searchParams] = useSearchParams()
-    console.log(searchParams.get('id'))
-
-    return(<div>{searchParams.get('id')}</div>)
+    const logement_id = searchParams.get('id')
+    const [ data, setData ] = useState(logements.find(item => item.id === logement_id))
+    
+    return(
+        <div>
+        <Carousel imgs={data.pictures}/>
+        {data.title}
+        </div>)
 }
